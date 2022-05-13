3236cc335f35c2bc7108ec17127e0c309faabff0
Green-Tea <sittingducks.lorenzo@gmail.com>
2022-05-13 22:52:23 +0700

    Added A2

---
+++ A2/misc/UseLib.scala
@@ -0,0 +1,7 @@
+object UseLib extends App {
+  def onlyBeginsWithLower(xs: Vector[String]): Vector[String] = ???
+
+  def longestString(xs: Vector[String]): Option[String] = ???
+
+  def longestLowercase(xs: Vector[String]): Option[String] = ???
+}
