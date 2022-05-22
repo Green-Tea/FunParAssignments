b4020dda9756abe3d6010d9cbf4b80b2e7deed7b
Green-Tea <sittingducks.lorenzo@gmail.com>
2022-05-22 21:39:38 +0700

    Update UseLib.scala

--- A2/misc/UseLib.scala
+++ A2/misc/UseLib.scala
@@ -1,7 +1,10 @@
 object UseLib extends App {
-  def onlyBeginsWithLower(xs: Vector[String]): Vector[String] = ???
+  def onlyBeginsWithLower(xs: Vector[String]): Vector[String] = {
+    val a = xs.filter(_.nonEmpty)
+    a.filter(_.charAt(0).isLower)
+  }
 
-  def longestString(xs: Vector[String]): Option[String] = ???
+  def longestString(xs: Vector[String]): Option[String] = if xs.isEmpty then None else Option(xs.maxBy(word => word.length))
 
   def longestLowercase(xs: Vector[String]): Option[String] = ???
 }
