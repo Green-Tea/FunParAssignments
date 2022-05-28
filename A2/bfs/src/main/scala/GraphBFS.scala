0c690d4e4fdfd168e235b0e5b0fd3dfc55da6eae
Green-Tea <sittingducks.lorenzo@gmail.com>
2022-05-28 15:42:15 +0700

    dateUtil and graphBfs done

--- A2/bfs/src/main/scala/GraphBFS.scala
+++ A2/bfs/src/main/scala/GraphBFS.scala
@@ -1,18 +1,43 @@
-object GraphBFS {
+import scala.annotation.tailrec
 
-  def bfs[V](nbrs: V => Set[V], src: V) = {
+object GraphBFS extends App {
 
-    def expand(frontier: Set[V], parent: Map[V, V]) = ???
+  def nbrs(node: String): Set[String] = {
+    if node.equals("A") then Set("B","C")
+    else if node.equals("B") then Set("A","D")
+    else if node.equals("C") then Set("A","E")
+    else if node.equals("D") then Set("B","F")
+    else if node.equals("E") then Set("C","F")
+    else if node.equals("F") then Set("D", "E")
+    else Set()
+  }
 
-    def iterate(frontier: Set[V], 
-                parent: Map[V, V], 
+  def bfs[V](nbrs: V => Set[V], src: V): (Map[V, V], Map[V, Int]) = {
+
+    def expand(frontier: Set[V], parent: Map[V, V]): (Set[V], Map[V, V]) = {
+      val newFrontier = frontier.flatMap(nbrs(_).filterNot(parent.contains))
+
+      @tailrec
+      def helper(front: Set[V], result: Map[V, V]): Map[V,V] = {
+        if frontier.isEmpty then return result
+        val nextResult: Map[V,V] = result ++ nbrs(front.head).filterNot(parent.contains).foldLeft(Map[V, V]()){(map, V) => map + (V -> front.head)}
+        helper(front.tail, nextResult)
+      }
+
+      val newParent: Map[V,V] = parent ++ helper(frontier, Map[V,V]())
+      (newFrontier, newParent)
+    }
+
+    @tailrec
+    def iterate(frontier: Set[V],
+                parent: Map[V, V],
                 distance: Map[V, Int], d: Int
                 ): (Map[V, V], Map[V, Int]) =
       if frontier.isEmpty then
         (parent, distance)
       else {
         val (frontier_, parent_) = expand(frontier, parent)
-        val distance_ = ???
+        val distance_ = distance ++ frontier.foldLeft(Map[V, Int]()){(map, V) => map + (V -> d)}
 
         iterate(frontier_, parent_, distance_, d + 1)
       }
@@ -20,4 +45,5 @@ object GraphBFS {
     iterate(Set(src), Map(src -> src), Map(), 0)
   }
 
+  println(bfs(nbrs, "A"))
 }
--- A2/misc/DateUtil.scala
+++ A2/misc/DateUtil.scala
@@ -30,19 +30,42 @@ object DateUtil extends App {
     months(d._2-1) + "-" +  d._1 + "-" + d._3
   }
 
-  def whatMonth(n: Int, yr: Int): Int = ???
+  def whatMonth(n: Int, yr: Int): Int = {
+    @tailrec
+    def findMonth(n: Int, dayInMonth: List[(Int,Int)]): Int = n match {
+      case n if n <= dayInMonth.head._2 => dayInMonth.head._1
+      case _ => findMonth(n-dayInMonth.head._2, dayInMonth.tail)
+    }
+    val LeapYr = List((1,31),(2,29),(3,31),(4,30),(5,31),(6,30),(7,31),(8,31),(9,30),(10,31),(11,30),(12,31))
+    val NormalYr = List((1,31),(2,28),(3,31),(4,30),(5,31),(6,30),(7,31),(8,31),(9,30),(10,31),(11,30),(12,31))
 
-  def oldest(dates: List[Date]): Option[Date] = ???
+    if (yr%4 == 0 || yr%400 ==0)&&(yr%100 != 0) then findMonth(n,LeapYr)
+    else findMonth(n,NormalYr)
+  }
 
-  def isReasonableDate(d: Date): Boolean = ???
+//  def oldest(dates: List[Date]): Option[Date] = dates match {
+//    case Nil => None
+//    case _ => dates.foldLeft(dates.head)((x, y) => isOlder(x, y))
+//  }
 
+  def isReasonableDate(d: Date): Boolean = d match{
+    case d if d._3 < 1 || d._2 < 1 || d._2 > 12 || d._1 < 1 => false
+    case d if (d._3%4 == 0 || d._3%400 ==0)&&(d._3%100 != 0) =>
+      val daysInMonthLeapYr = List(31,29,31,30,31,30,31,31,30,31,30,31)
+      d._1 <= daysInMonthLeapYr(d._2)
+    case d if (d._3%4 == 0 || d._3%400 ==0)&&(d._3%100 != 0) =>
+      val daysInMonthNormalYr = List(31,28,31,30,31,30,31,31,30,31,30,31)
+      d._1 <= daysInMonthNormalYr(d._2)
+    case _ => false
+
+  }
 //  println(isOlder((21,5,2022), (20,5,2002)))
 //  println(numberInMonth(List((1,2,2011), (2,12,2011), (4,2,2011)),3))
 //  println(numberInMonths( List((1,2,2011), (2,12,2011), (4,2,2011)) , List(2, 12)))
 //  println(datesInMonth( List((1,2,2011), (2,12,2011), (4,2,2011), (2,3,2011), (10,2,2011)), 2))
 //  println(datesInMonths( List((1,2,2011), (2,12,2011), (4,2,2011), (2,3,2011), (10,2,2011), (4,8,2011), (2,7,2011), (10,9,2011)), List(2,7)))
 //  println(dateToString(12,5,2014))
-//  println(whatMonth(61,2016))
+  println(whatMonth(61,2016))
 //  println(oldest( List((1,1,2001), (51,10,2015), (12,4,2001))))
-//  println(isReasonableDate( (29,2,4) ))
+  println(isReasonableDate( (30,2,4) ))
 }
